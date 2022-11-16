991eb08986e20d3a748198ea88c4917738bd00b5
Amalie <Amal956e@stud.kea.dk>
2022-11-16 20:43:46 +0100

    Tilføjet "app.get cafe" osv og ændret i cafe databse

--- app.js
+++ app.js
@@ -1,6 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require('dotenv');
 const mysqlConnection = require("./database");
+const port = 3000;
 
-app.get('/database-test', (req, res) => {
+//Reference til cafe.js
+//const cafes = require("./cafe");
+
+const URL_FOR_FRONTEND = "YOUR_GITHUB_PAGE_ORIGIN_HERE";
+
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
+const cors_url = process.env.NODE_ENV === "prod" ? URL_FOR_FRONTEND : "*";
+app.use(
+    cors({
+        origin: cors_url
+    })
+);
+
+//Endpoint til users
+app.get('/cafes', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM cafes;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+app.get('/Favorites', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM Favorites;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+app.get('/users', (req, res) => {
     mysqlConnection.query(
         "SELECT * FROM users;",
         (err, results, fields) => {
@@ -13,3 +60,47 @@ app.get('/database-test', (req, res) => {
     );
 })
 
+app.get('/product', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM product;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+app.get('/Cafe_product', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM Cafe_product;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+app.get('/Discribtion', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM Discribtion;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+
+
+app.listen(port, () => { //Udskriver http://localhost:3000
+    console.log(`Node.js REST API listening at http://localhost:${port}`);
+});
--- cafe database.sql
+++ cafe database.sql
@@ -16,7 +16,7 @@ CREATE TABLE Users (
     PRIMARY KEY (User_id)
 );
 
-CREATE TABLE Cafees (	
+CREATE TABLE Cafes (	
 	Cafe_id INTEGER, 
 	City VARCHAR(100), 
 	Address VARCHAR(100), 
@@ -34,7 +34,7 @@ CREATE TABLE Discribtion (
     Wifi BOOLEAN,
     Study_discount BOOLEAN,
     PRIMARY KEY (Discribtion_id),
-    FOREIGN KEY (Cafe_id) REFERENCES Cafees(Cafe_id)
+    FOREIGN KEY (Cafe_id) REFERENCES Cafes(Cafe_id)
 );
 
 CREATE TABLE Favorites (    
@@ -42,7 +42,7 @@ CREATE TABLE Favorites (
     Cafe_id INTEGER,
     User_id INTEGER,
     PRIMARY KEY (Fav_id),
-    FOREIGN KEY (Cafe_id) REFERENCES Cafees(Cafe_id),
+    FOREIGN KEY (Cafe_id) REFERENCES Cafes(Cafe_id),
     FOREIGN KEY (User_id) REFERENCES Users(User_id)
 );
 
@@ -58,7 +58,7 @@ CREATE TABLE Cafe_product (
     Product_id INTEGER,
     Cafe_id INTEGER,
     FOREIGN KEY (Product_id) REFERENCES Product(Product_id),
-    FOREIGN KEY (Cafe_id) REFERENCES Cafees(Cafe_id)
+    FOREIGN KEY (Cafe_id) REFERENCES Cafes(Cafe_id)
 );
 
 Insert into Users (user_id,user_name,study_card) values (1,'Amalie', true);
@@ -66,31 +66,31 @@ Insert into Users (user_id,user_name,study_card) values (2,'Maheen', true);
 Insert into Users (user_id,user_name,study_card) values (3,'Klara', true);
 Insert into Users (user_id,user_name,study_card) values (4,'Mille', false);
 
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (1,'Paludans bogcafe','Copenhagen','Fiolstræde 10, 1171 København');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (2,'Bastard cafe',"Copenhagen",'Rådhusstræde 13, 1466 København K');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (3,'Starbucks',"Copenhagen", 'Falkoner Alle, Frederiksberg Centret 21, 2000 Frederiksberg');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (4,'Coffee Collective',"Copenhagen", 'Sankt Hans Torv 3, 2200 København');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (5,'Lagekagehuset',"Copenhagen", 'Frederiksberggade 21, 1459 København K');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (6,'Café Vintage',"Copenhagen", 'Sløjfen 2, 2000 Frederiksberg');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (7,'Kaffehuset',"Copenhagen", 'Nørrebrogade 183, 2200 København');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (8,'Espresso House',"Copenhagen", 'Frederiksborggade 24, 1360 København');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (9,'Kaffestuen',"Copenhagen", 'Østerbrogade 150, 2100 København');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (10,'Brødflov ',"Copenhagen", 'Falkoner Alle 36, 2000 Frederiksberg');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (11,'Emmerys ',"Copenhagen", 'Nørre Voldgade, cnr, Frederiksborggade 15, 1360 København');
-Insert into Cafees (Cafe_id,Cafees_name,City,Address) values (12,'20 grams',"Copenhagen", 'Rosengården 13, 1174 København');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (1,'Paludans bogcafe','Copenhagen','Fiolstræde 10, 1171 København');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (2,'Bastard cafe',"Copenhagen",'Rådhusstræde 13, 1466 København K');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (3,'Starbucks',"Copenhagen", 'Falkoner Alle, Frederiksberg Centret 21, 2000 Frederiksberg');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (4,'Coffee Collective',"Copenhagen", 'Sankt Hans Torv 3, 2200 København');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (5,'Lagekagehuset',"Copenhagen", 'Frederiksberggade 21, 1459 København K');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (6,'Café Vintage',"Copenhagen", 'Sløjfen 2, 2000 Frederiksberg');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (7,'Kaffehuset',"Copenhagen", 'Nørrebrogade 183, 2200 København');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (8,'Espresso House',"Copenhagen", 'Frederiksborggade 24, 1360 København');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (9,'Kaffestuen',"Copenhagen", 'Østerbrogade 150, 2100 København');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (10,'Brødflov ',"Copenhagen", 'Falkoner Alle 36, 2000 Frederiksberg');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (11,'Emmerys ',"Copenhagen", 'Nørre Voldgade, cnr, Frederiksborggade 15, 1360 København');
+Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (12,'20 grams',"Copenhagen", 'Rosengården 13, 1174 København');
 
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (1,"low","small","cheap", 1, "low",true,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (2,"between","medium", "between",2, "medium",true,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (3,"high","big","expensive",3,"high",false,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (4,"low","small","cheap", 4, "low",true,false);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (5,"between","medium", "between",5, "medium",true,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (6,"high","big","expensive",6,"high",true,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (7,"low","small","cheap", 7, "low",true,false);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (8,"between","medium", "mellem",8, "medium",false,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (9,"high","big","expensive",9,"high",true,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (10,"high","big","expensive",10,"low",true,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (11,"between","medium", "between",8, "mediums",false,true);
-Insert into Discribtion (Discribtion_id, Sound_level, Cafee_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (12,"high","big","expensive",9,"high",true,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (1,"low","small","cheap", 1, "low",true,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (2,"between","medium", "between",2, "medium",true,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (3,"high","big","expensive",3,"high",false,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (4,"low","small","cheap", 4, "low",true,false);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (5,"between","medium", "between",5, "medium",true,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (6,"high","big","expensive",6,"high",true,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (7,"low","small","cheap", 7, "low",true,false);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (8,"between","medium", "mellem",8, "medium",false,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (9,"high","big","expensive",9,"high",true,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (10,"high","big","expensive",10,"low",true,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (11,"between","medium", "between",8, "mediums",false,true);
+Insert into Discribtion (Discribtion_id, Sound_level, Cafe_size, Price_range, Cafe_id, Lightning_level, Wifi, Study_discount) values (12,"high","big","expensive",9,"high",true,true);
 
 insert into Favorites (Fav_id, Cafe_id, User_id) values (1, 4, 1);
 insert into Favorites (Fav_id, Cafe_id, User_id) values (2, 12,2);
