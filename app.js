5d1b69271c7666e6ca51fe457dd06f8157687ce4
Amalie <Amal956e@stud.kea.dk>
2022-11-17 09:14:20 +0100

    Tilføjet "app.get cafe og de andre"

--- app.js
+++ app.js
@@ -18,9 +18,98 @@ app.use(
 );
 
 //Endpoint til cafes
-app.get('/cafes', (req, res) => {
+app.get('/Cafes', (req, res) => {
     mysqlConnection.query(
         "SELECT * FROM cafes;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+//Endpoint til Favorites
+app.get('/Favorites', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM Favorites;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+//Endpoint til users
+app.get('/users', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM users;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+//Endpoint til product
+app.get('/product', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM product;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+app.get("/product/:id", (req, res) => {
+    const id = req.params.id;
+    let allProducts = [];
+    console.log(id)
+    mysqlConnection.query(
+        "SELECT * FROM product;",
+        (err, results, fields) => {
+            if (!err) {
+                return res.json(product);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+    const product = allProducts.find(item => item.id == id);
+    console.log(allProducts)
+    return res.json(product);
+});
+
+//Endpoint til cafe_product
+app.get('/Cafe_product', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM Cafe_product;",
+        (err, results, fields) => {
+            if (!err) {
+                res.json(results);
+            } else {
+                console.log(err);
+            }
+        }
+    );
+})
+
+//Endpoint til Discribtion
+app.get('/Discribtion', (req, res) => {
+    mysqlConnection.query(
+        "SELECT * FROM Discribtion;",
         (err, results, fields) => {
             if (!err) {
                 res.json(results);
