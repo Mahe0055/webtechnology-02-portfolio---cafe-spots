54ae70d3d75076e0ff9c8214220f40af92577d07
Amalie <Amal956e@stud.kea.dk>
2022-11-18 08:58:15 +0100

    Tilføjet create cafe og filtere i cafer via city

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3000;
 const URL_FOR_FRONTEND = "YOUR_GITHUB_PAGE_ORIGIN_HERE";
 
 app.use(express.json()); //Used to parse JSON bodies
-app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+app.use(express.urlencoded({extended: true})); //Parse URL-encoded bodies
 
 const cors_url = process.env.NODE_ENV === "prod" ? URL_FOR_FRONTEND : "*";
 app.use(
@@ -17,7 +17,7 @@ app.use(
     })
 );
 
-//Endpoint til cafes
+//Endpoint til cafes. Returnerer alle cafeer i databasen
 app.get('/cafes', (req, res) => {
     mysqlConnection.query(
         "SELECT * FROM cafes;",
@@ -35,12 +35,40 @@ app.get('/cafes', (req, res) => {
 app.get('/cafes/:id', (req, res) => {
     const cafesParameter = req.params.id;
     const sql = 'SELECT * FROM `Cafes` WHERE `Cafe_id` = ?';
-    mysqlConnection.execute(sql,[cafesParameter], (err, results) => {
+    mysqlConnection.execute(sql, [cafesParameter], (err, results) => {
         res.send(results);
     })
 });
 
-//Endpoint til Favorites
+//Oprettelse af en ny cafe
+app.post('/cafes/create', (req, res,) => {
+    //Input værdierne i body'en
+    const inputCafeId = req.body.Cafe_id;
+    const inputCafeName = req.body.Cafees_name;
+    const inputCafeCity = req.body.City;
+    const inputCafeAdress = req.body.Address;
+
+    //Oprettelse af cafe og man sender post "send" via Postman
+    const sql = "Insert into Cafes (Cafe_id,Cafees_name,City,Address) values (?,?,?,?);"
+    mysqlConnection.execute(sql, [inputCafeId, inputCafeName, inputCafeCity, inputCafeAdress], (err, results) => {
+        res.send(results);
+    })
+
+    res.sendStatus(200); //Ok
+});
+
+//filtrere cafeer med query parameters
+app.get('/cafequery', (req, res,) => {
+    const city = req.query.city;
+
+    //cafequery?city=copenhagen sættes efter localhost:3000
+    const sql = 'SELECT * FROM `Cafes` WHERE `city` = ?';
+    mysqlConnection.execute(sql, [city], (err, results) => {
+        res.send(results);
+    })
+});
+
+//Endpoint til Favorites. Returnerer alle Favorites i databasen
 app.get('/Favorites', (req, res) => {
     mysqlConnection.query(
         "SELECT * FROM Favorites;",
@@ -58,12 +86,12 @@ app.get('/Favorites', (req, res) => {
 app.get('/favorites/:id', (req, res) => {
     const favoritesParameter = req.params.id;
     const sql = 'SELECT * FROM `favorites` WHERE `fav_id` = ?';
-    mysqlConnection.execute(sql,[favoritesParameter], (err, results) => {
+    mysqlConnection.execute(sql, [favoritesParameter], (err, results) => {
         res.send(results);
     })
 });
 
-//Endpoint til users
+//Endpoint til users. Returnerer alle users i databasen
 app.get('/users', (req, res) => {
     mysqlConnection.query(
         "SELECT * FROM users;",
@@ -81,12 +109,12 @@ app.get('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
     const usersParameter = req.params.id;
     const sql = 'SELECT * FROM `users` WHERE `user_id` = ?';
-    mysqlConnection.execute(sql,[usersParameter], (err, results) => {
+    mysqlConnection.execute(sql, [usersParameter], (err, results) => {
         res.send(results);
     })
 });
 
-//Endpoint til product
+//Endpoint til product. Returnerer alle product i databasen
 app.get('/product', (req, res) => {
     mysqlConnection.query(
         "SELECT * FROM product;",
@@ -104,12 +132,12 @@ app.get('/product', (req, res) => {
 app.get('/product/:id', (req, res) => {
     const productParameter = req.params.id;
     const sql = 'SELECT * FROM `product` WHERE `Product_id` = ?';
-    mysqlConnection.execute(sql,[productParameter], (err, results) => {
+    mysqlConnection.execute(sql, [productParameter], (err, results) => {
         res.send(results);
     })
 });
 
-//Endpoint til cafe_product
+//Endpoint til cafe_product. Returnerer alle cafe_product i databasen
 app.get('/Cafe_product', (req, res) => {
     mysqlConnection.query(
         "SELECT * FROM Cafe_product;",
@@ -124,15 +152,16 @@ app.get('/Cafe_product', (req, res) => {
 })
 
 //Get cafe_product by id
-app.get('/Cafe_product/:id/:id', (req, res) => {
-    const Cafe_productParameter = req.params.id;
-    const sql = 'SELECT * FROM `Cafe_product` WHERE `product_id` and `cafe_id` = ?';
-    mysqlConnection.execute(sql,[Cafe_productParameter], (err, results) => {
+app.get('/Cafe_product/:productId/:cafeId', (req, res) => {
+    const productId = req.params.productId;
+    const cafeId = req.params.cafeId;
+    const sql = 'SELECT * FROM `Cafe_product` WHERE `product_id` = ? and `cafe_id` = ?';
+    mysqlConnection.execute(sql, [productId, cafeId], (err, results) => {
         res.send(results);
     })
 });
 
-//Endpoint til Discribtion
+//Endpoint til Discribtion. Returnerer alle discribtion i databasen
 app.get('/Discribtion', (req, res) => {
     mysqlConnection.query(
         "SELECT * FROM Discribtion;",
@@ -150,7 +179,7 @@ app.get('/Discribtion', (req, res) => {
 app.get('/discribtion/:id', (req, res) => {
     const discribtionParameter = req.params.id;
     const sql = 'SELECT * FROM `discribtion` WHERE `discribtion_id` = ?';
-    mysqlConnection.execute(sql,[discribtionParameter], (err, results) => {
+    mysqlConnection.execute(sql, [discribtionParameter], (err, results) => {
         res.send(results);
     })
 });
