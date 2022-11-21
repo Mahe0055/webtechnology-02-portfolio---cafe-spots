323e85071ee98570bbffce9892b7e84a59b81fde
Amalie <Amal956e@stud.kea.dk>
2022-11-21 09:57:58 +0100

    Hjemmeside og main.js

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 fetch('http://localhost:3000/cafes')
     .then(response => response.json())
-    .then(function (wishes) {
-        renderWishes(wishes);
+    .then(function (cafeer) {
+        renderWishes(cafeer);
     });
 
-function renderWishes(wishes) {
-    console.log(wishes);
-    const ul = document.querySelector('ul#wishes');
+function renderWishes(cafeer) {
+    console.log(cafeer);
+    const ul = document.querySelector('ul#cafeer');
     console.log(ul);
-    for (let i = 0; i < wishes.length; i++) {
-        const wish = wishes[i];
-        console.log(wish);
+    for (let i = 0; i < cafeer.length; i++) {
+        const cafe = cafeer[i];
+        console.log(cafe);
         const li = document.createElement('li');
         li.innerHTML = `
-            <p class="text">${wish.text}</p>
-            <p class="price">${wish.price}</p>
+            <p class="text">${cafe.Cafees_name}</p>
         `;
         ul.appendChild(li);
     }
